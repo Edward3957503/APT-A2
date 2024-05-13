5c87992b28941b181ada156c25930b2a87480f00
Esmat5 <s3941885@student.rmit.edu.au>
2024-05-13 10:40:57 +1000

    Reorder node constructor data() with next()

--- Node.cpp
+++ Node.cpp
@@ -1,7 +1,7 @@
 #include "Node.h"
 
 // Constructor definition
-Node::Node() : next(nullptr), data(nullptr) {}
+Node::Node() : data(nullptr), next(nullptr) {}
 
 Node::~Node(){
     delete data; // Make sure to delete the allocated Stock object to avoid memory leaks
