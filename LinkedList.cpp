48a8489fbf1fd222dacf3901e752bef841ce60e0
Esmat5 <s3941885@student.rmit.edu.au>
2024-05-16 10:15:02 +1000

    Deleted unused a line of code in deleteFoodById function

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -241,7 +241,6 @@ void LinkedList::deleteFoodById() {
     bool quit = false;
     std::string id = "";
     std::cout << "Enter the food id of the food to remove from the menu: ";
-    // std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     if (!std::getline(std::cin, id) || id.empty() || std::cin.eof()){
         quit = true;
